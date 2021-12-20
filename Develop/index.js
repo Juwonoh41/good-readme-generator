cc3c0ec7912cc51111fb1239e7007816f43115d8
Juwon Oh <edmaster41@gmail.com>
2021-12-20 14:44:36 -0800

    Inquirer prompts

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,57 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer')
+const fs = require('fs')
+const generateMarkdown = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
-const questions = [];
-
+const questions = [ 
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter the title.'
+    },
+    {
+        type: 'input',
+        name: 'descriptions',
+        message: 'Enter the description.'
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'Enter the installation requirements.'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter the usage information.'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Enter the contribution guidelines.'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Enter the test guidelines.'
+    },
+    {
+        type: 'list',
+        message: 'Please select a license',
+        name: 'license',
+        choices: ["ISC", "MIT", "Apache", "GNU GPLv3"]
+    }
+];
+c
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+    
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+}
 
 // Function call to initialize app
 init();
